8a69cd3dd24f40d664238231c944c2dcf0637218
nguyenha265 <nguyenha2651988@gmail.com>
2019-07-25 14:35:30 +0700

    thuc hanh

---
+++ src/controllers/GreetingController.java
@@ -0,0 +1,16 @@
+package controllers;
+    import org.springframework.stereotype.Controller;
+    import org.springframework.ui.Model;
+    import org.springframework.web.bind.annotation.GetMapping;
+
+
+@Controller
+    public class GreetingController {
+        @GetMapping("/greeting")
+        public String greeting(Model model){
+            String name = "hello";
+            model.addAttribute("name",name);
+            return "index";
+        }
+    }
+
